4d26641baeb1822306e33e1d4de5a7a515fc7fde
GGerbit <gosha2007naz@gmail.com>
2022-07-21 15:12:07 +0300

    init repository

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+
+import router from './routes/servers.js'
+
+
+const PORT = process.env.PORT ?? 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded( { extended: false } ))
+// app.use("/api", router)
+
+
+app.get('/', (req, res) => {
+	res.send("<h1>OK</h1>")
+})
+
+
+app.listen(PORT, () => {
+	console.log(`server has been started on ${PORT} port...`)
+})
