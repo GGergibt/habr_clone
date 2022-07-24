d1f363b544c258f7aa9d20d79b505371db826cac
GGerbit <gosha2007naz@gmail.com>
2022-07-24 03:11:38 +0300

    change some middleware logic

--- routes/servers.js
+++ routes/servers.js
@@ -2,13 +2,13 @@ import {Router} from 'express';
 
 import userRouter from './users.js'
 
-import {validateEmail, validateUserFields} from '../middlewares/validateRequestMiddlewares.js'
+import postRouter from './posts.js'
+
 
 const router = Router()
 
-await router.use(validateUserFields)
-await router.use(validateEmail)
 await router.use("/users", userRouter)
+await router.use("/posts", postRouter)
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,17 @@ import {Router} from 'express';
 import {createUser, loginUser} from '../controllers/users.js'
 import {isPasswordValid} from '../middlewares/validateRequestMiddlewares.js'
 
+import {validateEmail, validateUserFields} from '../middlewares/validateRequestMiddlewares.js'
+
 
 
 const router = Router()
 
-
-await router.post("/create", createUser)
-
 await router.post("/login", isPasswordValid, loginUser)
+
+
+await router.post("/create", validateEmail, validateUserFields, createUser)
+
 
 export default router
 
