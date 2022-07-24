2786029b50c0055b3514f3b17aa760e61cc8051b
GGerbit <gosha2007naz@gmail.com>
2022-07-24 03:07:22 +0300

    add user id in jwt token

--- utils/generateJWT.js
+++ utils/generateJWT.js
@@ -1,8 +1,12 @@
 import jwt from 'jsonwebtoken';
+import {query} from '../connectDB.js'
 
 // export const generateAccessToken = async (name) => {
 // 	return await jwt.sign({username: name}, process.env.TOKEN_SECRET, { expiresIn: '180000s' });
 // }
-export const generateAccessToken = (name) => {
-	return jwt.sign({username: name}, process.env.TOKEN_SECRET, { expiresIn: '180000s' });
+export const generateAccessToken = async (name) => {
+	const response = await query(`SELECT id from user_table WHERE username='${name}'`)
+	const id = response[0].id
+
+	return jwt.sign({id: id, username: name}, process.env.TOKEN_SECRET, { expiresIn: '1800000s' });
 }
