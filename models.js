59271ac9c3f3caf4ad7ff8934e129ebc3d9f7647
GGerbit <gosha2007naz@gmail.com>
2022-07-25 03:10:38 +0300

    creating models db file. user model and post model

---
+++ models.js
@@ -0,0 +1,3 @@
+export const user_table = "CREATE TABLE IF NOT EXISTS `user_table` (`id` int NOT NULL AUTO_INCREMENT, `username` varchar(255) NOT NULL, `password` varchar(255) NOT NULL, `email` varchar(255) NOT NULL, `role` varchar(64) NOT NULL, UNIQUE KEY `id` (`id`), UNIQUE KEY `username` (`username`));"
+
+export const posts = "CREATE TABLE IF NOT EXISTS `posts` (`id` int NOT NULL AUTO_INCREMENT, `title` varchar(64) NOT NULL, `content` text NOT NULL, `image_destination` varchar(256) DEFAULT NULL, `author_id` int DEFAULT NULL, UNIQUE KEY `id` (`id`), KEY `author_id` (`author_id`), CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `user_table` (`id`));"
