54c721eaf0310cebe40b5f4cbbc013755224506a
GGerbit <gosha2007naz@gmail.com>
2022-07-25 03:08:11 +0300

    add config file for db

---
+++ db/config.js
@@ -0,0 +1,15 @@
+export const config = {
+	create_db: {
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		port: process.env.DB_PORT
+	},
+	db: {
+		host: process.env.DB_HOST, 
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		port: process.env.DB_PORT
+	}
+}
