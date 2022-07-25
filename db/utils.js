292cabab9d5bc2fbf86ba1c0218981c5ab66fef6
GGerbit <gosha2007naz@gmail.com>
2022-07-25 03:11:14 +0300

    add some utils db logic. create db, get session, create tables via models

---
+++ db/utils.js
@@ -0,0 +1,22 @@
+import {config} from './config.js';
+
+import mysql from 'mysql2/promise';
+
+import {posts, user_table} from '../models.js';
+
+export const createDB = async () => {
+	console.log(process.env.DB_NAME, "create")
+	const connection = await mysql.createConnection(config.create_db);
+	await connection.execute(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`)
+	return connection
+}
+
+export const getSession = async () => {
+	const connection = await mysql.createConnection(config.db)
+	return connection
+}
+export const createTables = async (connection) => {
+	await connection.execute(user_table)
+	await connection.execute(posts)
+
+}
