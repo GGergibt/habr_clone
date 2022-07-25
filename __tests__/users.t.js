e11ea6d88bb4c2133e67f55b6915829e8bf0643d
GGerbit <gosha2007naz@gmail.com>
2022-07-25 19:20:50 +0300

    add catch empty fields error

--- __tests__/users.t.js
+++ __tests__/users.t.js
@@ -22,20 +22,26 @@ const user_model = {
 	password: "123",
 	email: "user@ex.com"
 }
+
 const emailValidationError = async (email) => {
 	user_model.email = email
 	const res = await request(app).post('/api/users/create').send(user_model)
-	console.log(email, res.body.msg)
 	expect(res.statusCode).toBe(400);
 	expect(res.body.msg).toBeTruthy()
 	expect(res.body.msg).toBe("not valid email")
 }
 
+const emptyFieldError = async (model) => {
+	const res = await request(app).post('/api/users/create').send(model)
+	expect(res.statusCode).toBe(400)
+	expect(res.body.msg).toBeTruthy()
+	expect(res.body.msg).toBe("fields required")
+
+}
+
 describe('test creating user route', () => {
 	test('create user', async () => {
 		const res = await request(app).post('/api/users/create').send(user_model)
-		console.log(res.body.token)
-		console.log(process.env.DB_NAME)
 		expect(res.statusCode).toBe(201);
 		expect(res.body.token).toBeTruthy()
 
@@ -43,11 +49,22 @@ describe('test creating user route', () => {
 
 	test('catch duplicate user', async () => {
 		const res = await request(app).post('/api/users/create').send(user_model)
-		console.log(res.body.msg)
 		expect(res.statusCode).toBe(400);
 		expect(res.body.msg).toBeTruthy()
 		expect(res.body.msg).toBe("duplicate error. already exists")
 	
+
+	})
+	test('catch empty field error', async () => {
+		user_model.password = ""
+
+		await emptyFieldError(user_model)
+		user_model.password = "123"
+		const modelWithoutEmail = {
+			username: "fsdefs",
+			password: "123"
+		}
+		await emptyFieldError(modelWithoutEmail)
 
 	})
 	test ('catch validation email field error', async () => {
@@ -57,6 +74,7 @@ describe('test creating user route', () => {
 
 	})
 })
+
 afterAll(done => {
   connectionCreate.execute(`DROP DATABASE ${process.env.DB_NAME}`)
   done()
