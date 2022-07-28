372884955cbf2929e3a197981463b369783c0459
GGerbit <gosha2007naz@gmail.com>
2022-07-28 03:34:04 +0300

    change likes_of_posts model. Add if exists condition. fix bug with wrong foreign keys

--- models.js
+++ models.js
@@ -2,4 +2,4 @@ export const user_table = "CREATE TABLE IF NOT EXISTS `user_table` (`id` int NOT
 
 export const posts = "CREATE TABLE IF NOT EXISTS `posts` (`id` int NOT NULL AUTO_INCREMENT, `title` varchar(64) NOT NULL, `content` text NOT NULL, `image_destination` varchar(256) DEFAULT NULL, `author_id` int DEFAULT NULL, UNIQUE KEY `id` (`id`), KEY `author_id` (`author_id`), CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `user_table` (`id`));"
 
-export const likes_of_posts = "CREATE TABLE likes_of_posts(post_id int, user_id int, FOREIGN KEY (post_id) REFERENCES user_table(id), FOREIGN KEY (user_id) REFERENCES posts(id))"
+export const likes_of_posts = "CREATE TABLE IF NOT EXISTS likes_of_posts(post_id int, user_id int, FOREIGN KEY (user_id) REFERENCES user_table(id), FOREIGN KEY (post_id) REFERENCES posts(id))"
