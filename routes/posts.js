5d02821f9ec0759e86ad90ae72eea6f8d83b10e3
GGerbit <gosha2007naz@gmail.com>
2022-07-28 03:38:55 +0300

    create likes for posts logic functional

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import {createPost, getPost, allPosts} from '../controllers/posts.js'
+import {createPost, getPost, allPosts, getLikesCount, likePost} from '../controllers/posts.js'
 import {authenticateToken} from '../middlewares/jwtMiddlewares.js'
 import {validatePostFields} from '../middlewares/validateRequestMiddlewares.js';
 
@@ -9,6 +9,11 @@ const router = Router()
 router.post("/create", authenticateToken, validatePostFields, createPost)
 
 router.get("/all", allPosts)
+
+router.post("/:id/like", authenticateToken, likePost)
+
+router.get("/:id/likes_count", getLikesCount)
+
 
 router.get("/:id", getPost)
 
