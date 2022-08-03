9ed568edfa9d1b4626ee4dc7068868a9817b1e0b
GGerbit <gosha2007naz@gmail.com>
2022-08-03 03:13:10 +0300

    create cors for frontend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 import express from 'express'
 
-
+import cors from 'cors'
 
 import router from './routes/mainRouter.js'
 
 const app = express()
+
+app.use(cors({
+	origin: ['http://localhost:3000']
+}));
+
 
 app.use(express.json())
 app.use(express.urlencoded( { extended: false } ))
