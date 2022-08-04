f6d01ee9b845930deae55368993d76dfaa9e922c
GGerbit <gosha2007naz@gmail.com>
2022-08-05 02:58:36 +0300

    change Public upload folder to public

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, 'Public')
+		cb(null, 'public')
 	},
 	filename: (req, file, cb) => {
 		const fileName = Date.now() + path.extname(file.originalname)
