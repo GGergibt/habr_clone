2fea59a8a9f033abd3cdc9e55bb9a95805826f1f
GGerbit <gosha2007naz@gmail.com>
2022-08-04 23:07:13 +0300

    create description column for post

---
+++ backend/migrations/sqls/20220804200417-addDescriptionColumnForPost-up.sql
@@ -0,0 +1,5 @@
+/* Replace with your SQL commands */
+
+ALTER TABLE posts
+ADD COLUMN description VARCHAR(256)
+
--- backend/models.js
+++ backend/models.js
@@ -1,5 +1,5 @@
 export const user_table = "CREATE TABLE IF NOT EXISTS `user_table` (`id` int NOT NULL AUTO_INCREMENT, `username` varchar(255) NOT NULL, `password` varchar(255) NOT NULL, `email` varchar(255) NOT NULL, `role` varchar(64) NOT NULL, UNIQUE KEY `id` (`id`), UNIQUE KEY `username` (`username`));"
 
-export const posts = "CREATE TABLE IF NOT EXISTS `posts` (`id` int NOT NULL AUTO_INCREMENT, `title` varchar(64) NOT NULL, `content` text NOT NULL, `image` varchar(256) DEFAULT NULL, `author_id` int DEFAULT NULL, `created_at` datetime default CURRENT_TIMESTAMP, UNIQUE KEY `id` (`id`), KEY `author_id` (`author_id`), CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `user_table` (`id`));"
+export const posts = "CREATE TABLE IF NOT EXISTS `posts` (`id` int NOT NULL AUTO_INCREMENT, `title` varchar(64) NOT NULL, `content` text NOT NULL, `description` varchar(256), `image` varchar(256) DEFAULT NULL, `author_id` int DEFAULT NULL, `created_at` datetime default CURRENT_TIMESTAMP, UNIQUE KEY `id` (`id`), KEY `author_id` (`author_id`), CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `user_table` (`id`));"
 
 export const likes_of_posts = "CREATE TABLE IF NOT EXISTS likes_of_posts(post_id int, user_id int, FOREIGN KEY (user_id) REFERENCES user_table(id), FOREIGN KEY (post_id) REFERENCES posts(id))"
