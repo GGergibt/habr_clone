414c094930f860cd27d306cd303d77cd02747a7e
GGerbit <gosha2007naz@gmail.com>
2022-08-05 20:46:13 +0300

    add in base url post prefix

--- frontend/src/store/post.api.ts
+++ frontend/src/store/post.api.ts
@@ -6,19 +6,19 @@ import {ServerResponse, IPost} from '../models/models'
 export const postApi = createApi({
 	reducerPath: 'post',
 	baseQuery: fetchBaseQuery({
-		baseUrl: "http://localhost:8000/api"
+		baseUrl: "http://localhost:8000/api/post"
 	}),
 
 	endpoints: build => ({
 		allPosts: build.query<IPost[], any>({
 			query: () => ({
-				url: 'post/all'
+				url: 'all'
 			}),
 			transformResponse: (response: ServerResponse) => response.posts
 		}),
 		getPost: build.query<IPost, number>({
 			query: (id: number) => ({
-				url: `post/${id}`
+				url: `${id}`
 			}),
 			transformResponse: (response: ServerResponse) => response.post
 		})
