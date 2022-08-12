40744146ccfc8b8e9ccf9dbba370dbc15dff9d51
GGerbit <gosha2007naz@gmail.com>
2022-08-13 02:27:48 +0300

    delete useless imports

--- frontend/src/components/postForm.tsx
+++ frontend/src/components/postForm.tsx
@@ -1,17 +1,11 @@
 import {useSendForm} from '../hooks/useSendForm'
 import {useCreatePostMutation} from '../store/post.api'
-import {useParams} from 'react-router-dom'
-import {useCookies} from 'react-cookie'
-import {useEffect} from 'react'
 import {useEditPost} from '../hooks/useEditPost'
 
 const PostForm = () => {
-	const { id } = useParams()
-	const [cookies, setCookies] = useCookies()
 	const [createPost, {isLoading, isError, data: response, error}] = useCreatePostMutation({})
 	const sendForm = useSendForm(createPost)
 	const isEdit = useEditPost(sendForm.setValue)
-	console.log(isEdit)
 
 	return (
 
