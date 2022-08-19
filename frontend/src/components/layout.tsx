c51619291226fe5b40a963b2806053b9e0b44eb1
GGerbit <gosha2007naz@gmail.com>
2022-08-20 02:23:59 +0300

    create signout in layout

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,19 +1,29 @@
 import {Link, Outlet} from 'react-router-dom'
 import {useAuth} from '../hooks/useAuth'
+import {useSignout} from '../hooks/useSignout'
 
 const Layout = () => {
 
-	const {isAuthorizited} = useAuth()
+	const user = useAuth()
+	const signout = useSignout()
+	console.log(user.isAuthorizited)
+	console.log(typeof user.isAuthorizited)
 	return (
 		<>
 			<div>
 				
-			{ isAuthorizited ?? 
+			{ user.isAuthorizited ??
 				<>	
 				<Link to="/signup">Sign up</Link>
 				<Link to="/login">login</Link>
 				</>
 			}
+				
+			{user.isAuthorizited && 
+				<form onSubmit={signout}>
+					<button type="submit">Sign out</button>
+				</form>
+					}
 			</div>
 
 
--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -2,7 +2,7 @@ import {useCookies} from 'react-cookie'
 export const useAuth = () => {
 	const [cookies, setCookies] = useCookies()
 
-	const isAuthorizited = cookies.token ? true : false
+	const isAuthorizited = cookies.token ? true : null
 
 	return {
 		isAuthorizited: isAuthorizited
