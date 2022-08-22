8c013b88d8da28d45c2110e757d8a472a3f49e2a
GGerbit <gosha2007naz@gmail.com>
2022-08-23 00:38:15 +0300

    create image upload

--- frontend/src/components/postForm.tsx
+++ frontend/src/components/postForm.tsx
@@ -1,21 +1,30 @@
 import {useSendForm} from '../hooks/useSendForm'
 import {useCreatePostMutation} from '../store/post.api'
 import {useEditPost} from '../hooks/useEditPost'
+import {useCookies} from 'react-cookie'
+import {useEffect} from 'react'
 
 const PostForm = () => {
 	const [createPost, {isLoading, isError, data: response, error}] = useCreatePostMutation({})
+	const [cookies, setCookies] = useCookies()
 	const sendForm = useSendForm(createPost)
 	const isEdit = useEditPost(sendForm.setValue)
+	console.log(error)
+
 
 	return (
 
 	  <>
-	  <form onSubmit={sendForm.handleSubmit(sendForm.onSubmit)}>
+	  <form encType="mutlipart/form-data" onSubmit={sendForm.handleSubmit(sendForm.onSubmit)} >
 
               <div>
 		      
                   <label>Title </label>
 		      <input {...sendForm.register('title', {required: "название обязательно"})} placeholder="Название"/>
+              </div>
+              <div>
+                  <label>Image </label>
+		      <input type="file" {...sendForm.register('image', {required: "Контент обязателен"})} placeholder="image"/>
               </div>
               <div>
                   <label>Content </label>
