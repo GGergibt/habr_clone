126213fb9bcde7dbf4a8b07d40b00152a03abe2a
GGerbit <gosha2007naz@gmail.com>
2022-08-26 02:05:05 +0300

    add styles to login/signup pages

--- frontend/src/components/loginForm.tsx
+++ frontend/src/components/loginForm.tsx
@@ -14,20 +14,21 @@ export const LoginForm = () => {
 	  <>
 	  <form onSubmit={sendForm.handleSubmit(sendForm.onSubmit)}>
 
-          <div>
+          <div className="grid gap-6 mb-6 md:grid-cols-2">
               <div>
 		      
                   <label>Username </label>
-		      <input {...sendForm.register('username', {required: "Имя обязательно"})} placeholder="User Name"/>
+		      <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...sendForm.register('username', {required: "Имя обязательно"})} placeholder="User Name"/>
               </div>
               <div>
                   <label>Password </label>
-		      <input {...sendForm.register('password', {required: "Пароль обязателен"})} type="password"  id="password" placeholder="Password"/>
+		      <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...sendForm.register('password', {required: "Пароль обязателен"})} type="password"  id="password" placeholder="Password"/>
               </div>
           </div>
-          <div>
+          <div className="flex items-start mb-6">
 
-              <button type="submit">Войти</button>
+<button type="submit" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Войти</button>
+
           </div>
 
 	  </form>
--- frontend/src/components/signUpForm.tsx
+++ frontend/src/components/signUpForm.tsx
@@ -1,4 +1,5 @@
 import {useSignUpMutation} from '../store/user.api'
+import {useEffect} from 'react'
 
 import {useSendForm} from '../hooks/useSendForm'
 
@@ -11,33 +12,36 @@ export const SignUpForm = () => {
 	const password = sendForm.watch("password", "")
 	const confirmPassword = sendForm.watch("confirmPassword", "")
 
+
 	const cookies = useCreateCookieToken(response)
+
 
 
 	return (
 	  <>
 	  <form onSubmit={sendForm.handleSubmit(sendForm.onSubmit)}>
 
-          <div>
+          <div className="grid gap-6 mb-6 md:grid-cols-2">
               <div>
 		      
-                  <label>Username </label>
-		      <input {...sendForm.register('username', {required: "Имя обязательно"})} placeholder="User Name"/>
+                  <label>Имя пользователя</label>
+		      <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...sendForm.register('username', {required: "Имя обязательно"})} placeholder="UserName"/>
               </div>
               <div>
-                  <label>Email </label>
-		      <input {...sendForm.register('email', {required: "почта обязательна"})} type="email" id="email" placeholder="Email"/>
+                  <label>Почта </label>
+		      <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...sendForm.register('email', {required: "почта обязательна"})} type="email" id="email" placeholder="user@example.com"/>
               </div>
-              <div>
-                  <label>Password </label>
-		      <input {...sendForm.register('password', {required: "Пароль обязателен"})} type="password"  id="password" placeholder="Password"/>
-              </div>
-                   <label>Confirm Password </label>
-		  <input {...sendForm.register('confirmPassword', {required: true, validate: value => value === password || "password do not match"})} type="password" id="confirmPassword" placeholder="Confirm Password"/>
+	      <div className="mb-6">
+
+                  <label>Пароль </label>
+		      <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...sendForm.register('password', {required: "Пароль обязателен"})} type="password"  id="password" placeholder="*********"/>
+                   <label>Подтвердить пароль</label>
+		  <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...sendForm.register('confirmPassword', {required: true, validate: value => value === password || "password do not match"})} type="password" id="confirmPassword" placeholder="*********"/>
+          </div>
           </div>
           <div>
 
-              <button type="submit">Sign up</button>
+              <button type="submit" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Sign up</button>
           </div>
 
 	  </form>
