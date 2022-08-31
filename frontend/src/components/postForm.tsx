63bfd8f79b01f48715be8dfc4820b46b491735aa
GGerbit <gosha2007naz@gmail.com>
2022-08-31 03:25:14 +0300

    add ru names for inputs and button in post form

--- frontend/src/components/postForm.tsx
+++ frontend/src/components/postForm.tsx
@@ -20,20 +20,20 @@ const PostForm = () => {
 
               <div>
 		      
-                  <label>Title </label>
+                  <label>Название </label>
 		      <input  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...sendForm.register('title', {required: "название обязательно"})} placeholder="Название"/>
               </div>
               <div>
-                  <label>Image </label>
+                  <label>Изображение </label>
 		      <input type="file" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...sendForm.register('image', {required: "Контент обязателен"})} placeholder="image"/>
               </div>
               <div>
-                  <label>Content </label>
+                  <label>Контент</label>
 		      <input  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...sendForm.register('content', {required: "Контент обязателен"})} placeholder="Контент"/>
               </div>
           <div>
 
-              <button type="submit" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 my-3 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Sign up</button>
+              <button type="submit" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 my-3 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Создать</button>
           </div>
 
 
