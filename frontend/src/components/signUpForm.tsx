a58be2ed6b0f25e305e9539cc18667fbdc691861
GGerbit <gosha2007naz@gmail.com>
2022-09-06 22:05:04 +0300

    create alert if user already exists in sign up route

--- frontend/src/components/signUpForm.tsx
+++ frontend/src/components/signUpForm.tsx
@@ -1,13 +1,16 @@
 import {useSignUpMutation} from '../store/user.api'
 import {useNavigate} from 'react-router-dom'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 
 import {useSendForm} from '../hooks/useSendForm'
+import PopupPassword from './popupConfirmPasswordsError' 
 
 import {useCreateCookieToken} from '../hooks/useCreateCookie'
 
 export const SignUpForm = () => {
 	const [sendUser, {isLoading, isError, data: response, error}] = useSignUpMutation({})
+	const [isPasswordsCompares, setIsPasswordsCompares] = useState(true)
+
 
 	const sendForm = useSendForm(sendUser)
 	const navigate = useNavigate()
@@ -17,10 +20,21 @@ export const SignUpForm = () => {
 
 	const cookies = useCreateCookieToken(response)
 
+	const errorHandling = (errorResponse: any) => {
+		alert(errorResponse.msg)
+	}
+
 
 	useEffect(() => {
 		response && navigate('/')
 	}, [response])
+
+	useEffect(() => {
+	if (error && 'status' in error) {
+			errorHandling(error.data)
+	    }
+		}
+		 , [error])
 
 
 	return (
@@ -44,8 +58,11 @@ export const SignUpForm = () => {
                    <label>Подтвердить пароль</label>
 		  <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...sendForm.register('confirmPassword', {required: true, validate: value => value === password || "password do not match"})} type="password" id="confirmPassword" placeholder="*********"/>
           </div>
+
           </div>
           <div>
+
+
 
               <button type="submit" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Sign up</button>
           </div>
