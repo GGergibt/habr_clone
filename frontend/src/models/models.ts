5db757b8fde31a311893007fe3b87153ccc47985
GGerbit <gosha2007naz@gmail.com>
2022-09-09 22:32:09 +0300

    add models

--- frontend/src/models/models.ts
+++ frontend/src/models/models.ts
@@ -7,8 +7,23 @@ author: string;
 description: string;
 likes_count: number;
 created_at: Date;
+token: string;
 }
 
+export interface IUser {
+username: string;
+email: string;
+password: string;
+confirmPassword: string;
+}
+
+export interface IToken {
+	token: string;
+}
+export interface IPostWithToken {
+	token: string;
+	id: number
+}
 
 export interface ServerResponse {
 	posts: IPost[];
